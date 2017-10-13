6b222b821fe40a45f7ef339131a138fd6960ac46
Kai Kuehn <kuehn@diqa-pm.com>
2017-10-13 15:47:23 +0200

    merged from WiMa

--- proxy/src/SolrProxy/Auth.php
+++ proxy/src/SolrProxy/Auth.php
@@ -2,99 +2,136 @@
 namespace DIQA\SolrProxy;
 
 class Auth {
-	
-	public static function session() {
-		session_start();
-		
-		if (Auth::checkLogout()) {
-			@session_destroy();
-			exit;
-		}
-		
-		global $wgDBname;
-		$userid = self::getCookie($wgDBname.'UserID');
-		$userName = self::getCookie($wgDBname.'UserName');
-		
-		// access Wiki once to retrieve user groups and store it in a proxy-session
-		if (count(self::getSession('user_groups'.$userid)) === 0) {
-			$_SESSION['user_groups'.$userid] = [];
-			
-			$sessionId = self::getCookie($wgDBname.'_session');
-			$cookies = [
-					$wgDBname.'UserID' => $userid,
-					$wgDBname.'UserName' => $userName,
-					$wgDBname.'_session' => $sessionId
-			];
-			
-			global $wgServerHTTP, $wgScriptPath;
-			$res = self::http(
-					$wgServerHTTP . $wgScriptPath .
-					"/api.php?action=diqa_util_userdataapi&format=json",
-					$cookies);
-			$o = json_decode($res[2]);
-			$groups = isset($o->result->user_groups) ? $o->result->user_groups : [];
-			
-			$_SESSION['user_groups'.$userid] = $groups;
-		}
-		
-		return $_SESSION['user_groups'.$userid];
-	}
-	
-	public static function checkLogout() {
-		$parts = parse_url ( $_SERVER['REQUEST_URI'] );
-		$query = $parts ['query'];
-		$keyValues = explode ( "&", $query );
-		$params = [ ];
-	
-		foreach ( $keyValues as $keyValue ) {
-			list ( $key, $value ) = explode ( "=", $keyValue );
-			$params [$key] = urldecode ( $value );
-		}
-	
-		return isset($params['logout']);
-	}
-	
-	private static function http($url, $cookies) {
-		$res = "";
-		$header = "";
-		global $wgDBname;
-		
-	
-		// Create a curl handle to a non-existing location
-		$ch = curl_init($url);
 
-		$cookieArray = [];
-		foreach($cookies as $key => $value) {
-			$cookieArray[] = "$key=$value";
-		}
-		
-		
-		// Execute
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
-		curl_setopt($ch, CURLOPT_HEADER, true);
-		curl_setopt($ch, CURLOPT_COOKIE, implode('; ', $cookieArray));
-		$res = curl_exec($ch);
-	
-		$status = curl_getinfo($ch,CURLINFO_HTTP_CODE);
-		curl_close($ch);
-	
-		$bodyBegin = strpos($res, "\r\n\r\n");
-		list($header, $res) = $bodyBegin !== false ? array(substr($res, 0, $bodyBegin), substr($res, $bodyBegin+4)) : array($res, "");
-		return array($header, $status, str_replace("%0A%0D%0A%0D", "\r\n\r\n", $res));
-	}
-	
-	private static function getCookie($var) {
-		if (isset($_COOKIE[$var])) {
-			return $_COOKIE[$var];
-		}
-		return '';
-	}
-	
-	private static function getSession($var) {
-		if (isset($_SESSION[$var])) {
-			return $_SESSION[$var];
-		}
-		return [];
-	}
+    /**
+     *
+     * @return a list of groups for the currently logged-in user
+     */
+    public static function session() {
+        session_start();
+
+        if (Auth::checkLogout()) {
+            @session_destroy();
+            exit();
+        }
+
+        global $wgDBname;
+        $userid = self::getCookie($wgDBname . 'UserID');
+        $userName = self::getCookie($wgDBname . 'UserName');
+
+        // access Wiki once to retrieve user groups and store it in a proxy-session
+        if (count(self::getSession('user_groups' . $userid)) === 0) {
+            $_SESSION['user_groups' . $userid] = [];
+
+            $sessionId = self::getCookie($wgDBname . '_session');
+            $cookies = [
+                $wgDBname . 'UserID' => $userid,
+                $wgDBname . 'UserName' => $userName,
+                $wgDBname . '_session' => $sessionId
+            ];
+
+            global $wgServerHTTP, $wgScriptPath;
+            $res = self::http("$wgServerHTTP$wgScriptPath/api.php?action=diqa_util_userdataapi&format=json", $cookies);
+            
+            $o = json_decode($res[2]);
+            
+            if (isset($o->result)) {
+                $groups = isset($o->result->user_groups) ? $o->result->user_groups : [];
+                $_SESSION['user_groups' . $userid] = $groups;
+            } else {
+                @session_destroy();
+                throw new \Exception("Not logged in.");
+            }
+            
+        }
+
+        return $_SESSION['user_groups' . $userid];
+    }
+
+    /**
+     *
+     * @return true iff the user is logged in
+     */
+    public static function isLoggedIn() {
+        try {
+            Auth::session();
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public static function checkLogout() {
+        $parts = parse_url($_SERVER['REQUEST_URI']);
+        $query = $parts['query'];
+        $keyValues = explode("&", $query);
+        $params = [];
+
+        foreach ($keyValues as $keyValue) {
+            list ($key, $value) = explode("=", $keyValue);
+            $params[$key] = urldecode($value);
+        }
+
+        return isset($params['logout']);
+    }
+
+    /**
+     * calls the URL
+     *
+     * @return array with
+     *         $header
+     *         $status
+     *         $res (json-encoded)
+     */
+    private static function http($url, $cookies) {
+        $res = "";
+        $header = "";
+        global $wgDBname;
+
+        // Create a curl handle to a non-existing location
+        $ch = curl_init($url);
+
+        $cookieArray = [];
+        foreach ($cookies as $key => $value) {
+            $cookieArray[] = "$key=$value";
+        }
+
+        // Execute
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_COOKIE, implode('; ', $cookieArray));
+        $res = curl_exec($ch);
+
+        $status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        $bodyBegin = strpos($res, "\r\n\r\n");
+        list ($header, $res) = $bodyBegin !== false ? array(
+            substr($res, 0, $bodyBegin),
+            substr($res, $bodyBegin + 4)
+        ) : array(
+            $res,
+            ""
+        );
+        return array(
+            $header,
+            $status,
+            str_replace("%0A%0D%0A%0D", "\r\n\r\n", $res)
+        );
+    }
+
+    private static function getCookie($var) {
+        if (isset($_COOKIE[$var])) {
+            return $_COOKIE[$var];
+        }
+        return '';
+    }
+
+    private static function getSession($var) {
+        if (isset($_SESSION[$var])) {
+            return $_SESSION[$var];
+        }
+        return [];
+    }
 }
--- proxy/src/SolrProxy/SolrService.php
+++ proxy/src/SolrProxy/SolrService.php
@@ -1,171 +1,169 @@
 <?php
 namespace DIQA\SolrProxy;
 
-
-require_once( __DIR__ . '/../SolrPhpClient/Apache/Solr/Service.php');
+require_once (__DIR__ . '/../SolrPhpClient/Apache/Solr/Service.php');
 
 /**
- * This is a sub class of the Apache_Solr_Service. It adds an additional method
- * for sending raw queries to SOLR.
+ * This is a sub class of the Apache_Solr_Service.
+ * It adds an additional method for sending raw queries to SOLR.
  *
  * @author thsc
  *
  */
 class SolrService extends \Apache_Solr_Service {
-	
-	private $groups;
-	
-	/**
-	 * Constructor. All parameters are optional and will take on default values
-	 * if not specified.
-	 *
-	 * @param string $host
-	 * @param string $port
-	 * @param string $path
-	 * @param Boolean
-	 * @param string $userpass user:pass
-	 * @param Apache_Solr_HttpTransport_Interface $httpTransport
-	 */
-	public function __construct($host = 'localhost', $port = 8983, $path = '/solr/', $httpTransport = false, $userpass)
-	{
-		parent::__construct($host, $port, $path, $httpTransport, $userpass);
-		
-		$this->groups = Auth::session();
-		if (count($this->groups) === 0) {
-			throw new \Apache_Solr_InvalidArgumentException("Not logged in");
-		}		
-	}
-	
-	/**
-	 * Does a raw search on the SOLR server. The $queryString should have the
-	 * Lucene query format
-	 *
-	 * @param string $queryString The raw query string
-	 * @param string $method The HTTP method (Apache_Solr_Service::METHOD_GET or Apache_Solr_Service::METHOD::POST)
-	 * @return Apache_Solr_Response
-	 *
-	 * @throws Apache_Solr_HttpTransportException If an error occurs during the service call
-	 * @throws Apache_Solr_InvalidArgumentException If an invalid HTTP method is used
-	 */
-	public function rawsearch($queryString, $method = self::METHOD_GET)
-	{
-		
-		if ($method == self::METHOD_GET)
-		{
-			return $this->_sendRawGet($this->_searchUrl . $this->_queryDelimiter . $queryString);
-		}
-		else if ($method == self::METHOD_POST)
-		{
-			return $this->_sendRawPost($this->_searchUrl, $queryString, FALSE, 'application/x-www-form-urlencoded; charset=UTF-8');
-		}
-		else
-		{
-			throw new \Apache_Solr_InvalidArgumentException("Unsupported method '$method', please use the Apache_Solr_Service::METHOD_* constants");
-		}
-	}
-	
-	/**
-	 * Updates the search statistics in MW object cache.
-	 *
-	 * @param string $response SOLR response (JSONp)
-	 */
-	function updateSearchStats($response) {
-		$response = substr($response, strlen('_jqjsp('), -2);
-		$jsonResponse = json_decode($response);
-	
-		$numFound = $jsonResponse->response->numFound;
-		$params = '--searches';
-		if ($numFound > 0) {
-			$params .= ' --searchHits';
-		}
-	
-		BackgroundProcess::open("php " . __DIR__ . "/../../maintenance/updateSearchStats.php $params");
-	}
-	/**
-	 * Applies constraints depending on user groups.
-	 *
-	 * @param string $query
-	 * @return string
-	 */
-	function applyConstraints($query) {
-		global $userid, $userName;
-		global $fsgNamespaceConstraint, $fsgCustomConstraint;
-	
-		$userGroups = $this->groups;
-	
-		// namespace constraints
-		if (!isset($fsgNamespaceConstraint)) $fsgNamespaceConstraint = [];
-		foreach($fsgNamespaceConstraint as $group => $namespaces) {
-			if (in_array($group, $userGroups)) {
-				$constraints = [];
-				foreach($namespaces as $namespace) {
-					$constraints[] = "smwh_namespace_id:$namespace";
-				}
-					
-				$query = $query . "&fq=".urlencode(implode(' OR ', $constraints));
-			}
-		}
-	
-		// custom constraints
-		if (!isset($fsgCustomConstraint)) $fsgCustomConstraint = [];
-		foreach($fsgCustomConstraint as $operation) {
-			$query = $operation($query, $userGroups, $userName);
-		}
-		return $query;
-	
-	}
-	/**
-	 * Adds filter query parameters to main query parameters.
-	 * This is required for boosting.
-	 *
-	 * @param string $query
-	 * @return string
-	 */
-	function putFilterParamsToMainParams($query) {
-	
-		// parse query string
-		$parsedResults = [];
-		$params = explode("&", $query);
-		foreach($params as $p) {
-			$keyValue = explode("=", $p);
-			$parsedResults[$keyValue[0]][] = $keyValue[1];
-		}
-	
-		// add fq-params to q-params
-		if (isset($parsedResults['fq'])) {
-			foreach($parsedResults['fq'] as $fq) {
-				$parsedResults['q'][] = $fq;
-			}
-	
-		}
-	
-		// add boost dummy
-		$parsedResults['q'][] = 'smwh_boost_dummy%3A1';
-	
-		// serialize query string
-		$url = '';
-		$first = true;
-		foreach($parsedResults as $key => $values) {
-	
-			if ($key == 'q') {
-				if (!$first) {
-					$url .= '&';
-				}
-				$url .= "q=";
-				$url .= '(' . implode(' ) AND ( ', $values) . ')';
-				$first = false;
-			} else {
-				foreach($values as $val) {
-					if (!$first) {
-						$url .= '&';
-					}
-					$val = (string) $val;
-					$url .= "$key=$val";
-					$first = false;
-				}
-			}
-		}
-	
-		return $url;
-	}
+
+    private $groups;
+
+    /**
+     * Constructor.
+     * All parameters are optional and will take on default values
+     * if not specified.
+     *
+     * @param string $host
+     * @param string $port
+     * @param string $path
+     * @param boolean $httpTransport
+     * @param string $userpass of the form "user:pass"
+     */
+    public function __construct($host = 'localhost', $port = 8983, $path = '/solr/', $httpTransport = false, $userpass) {
+        parent::__construct($host, $port, $path, $httpTransport, $userpass);
+        
+        try {
+            $this->groups = Auth::session();
+        } catch (\Exception $e) {
+            throw new \Apache_Solr_InvalidArgumentException("Not logged in");
+        }
+    }
+
+    /**
+     * Does a raw search on the SOLR server.
+     * The $queryString should have the
+     * Lucene query format
+     *
+     * @param string $queryString
+     *            The raw query string
+     * @param string $method
+     *            The HTTP method (Apache_Solr_Service::METHOD_GET or Apache_Solr_Service::METHOD::POST)
+     * @return Apache_Solr_Response
+     *
+     * @throws Apache_Solr_HttpTransportException If an error occurs during the service call
+     * @throws Apache_Solr_InvalidArgumentException If an invalid HTTP method is used
+     */
+    public function rawsearch($queryString, $method = self::METHOD_GET) {
+        if ($method == self::METHOD_GET) {
+            return $this->_sendRawGet($this->_searchUrl . $this->_queryDelimiter . $queryString);
+        } else if ($method == self::METHOD_POST) {
+            return $this->_sendRawPost($this->_searchUrl, $queryString, FALSE, 'application/x-www-form-urlencoded; charset=UTF-8');
+        } else {
+            throw new \Apache_Solr_InvalidArgumentException("Unsupported method '$method', please use the Apache_Solr_Service::METHOD_* constants");
+        }
+    }
+
+    /**
+     * Updates the search statistics in MW object cache.
+     *
+     * @param string $response
+     *            SOLR response (JSONp)
+     */
+    function updateSearchStats($response) {
+        $response = substr($response, strlen('_jqjsp('), - 2);
+        $jsonResponse = json_decode($response);
+        
+        $numFound = $jsonResponse->response->numFound;
+        $params = '--searches';
+        if ($numFound > 0) {
+            $params .= ' --searchHits';
+        }
+        
+        BackgroundProcess::open("php " . __DIR__ . "/../../maintenance/updateSearchStats.php $params");
+    }
+
+    /**
+     * Applies constraints depending on user groups.
+     *
+     * @param string $query
+     * @return string
+     */
+    function applyConstraints($query) {
+        global $userid, $userName;
+        global $fsgNamespaceConstraint, $fsgCustomConstraint;
+        
+        $userGroups = $this->groups;
+        
+        // namespace constraints
+        if (! isset($fsgNamespaceConstraint))
+            $fsgNamespaceConstraint = [];
+        foreach ($fsgNamespaceConstraint as $group => $namespaces) {
+            if (in_array($group, $userGroups)) {
+                $constraints = [];
+                foreach ($namespaces as $namespace) {
+                    $constraints[] = "smwh_namespace_id:$namespace";
+                }
+                
+                $query = $query . "&fq=" . urlencode(implode(' OR ', $constraints));
+            }
+        }
+        
+        // custom constraints
+        if (! isset($fsgCustomConstraint))
+            $fsgCustomConstraint = [];
+        foreach ($fsgCustomConstraint as $operation) {
+            $query = $operation($query, $userGroups, $userName);
+        }
+        return $query;
+    }
+
+    /**
+     * Adds filter query parameters to main query parameters.
+     * This is required for boosting.
+     *
+     * @param string $query
+     * @return string
+     */
+    function putFilterParamsToMainParams($query) {
+        
+        // parse query string
+        $parsedResults = [];
+        $params = explode("&", $query);
+        foreach ($params as $p) {
+            $keyValue = explode("=", $p);
+            $parsedResults[$keyValue[0]][] = $keyValue[1];
+        }
+        
+        // add fq-params to q-params
+        if (isset($parsedResults['fq'])) {
+            foreach ($parsedResults['fq'] as $fq) {
+                $parsedResults['q'][] = $fq;
+            }
+        }
+        
+        // add boost dummy
+        $parsedResults['q'][] = 'smwh_boost_dummy%3A1';
+        
+        // serialize query string
+        $url = '';
+        $first = true;
+        foreach ($parsedResults as $key => $values) {
+            
+            if ($key == 'q') {
+                if (! $first) {
+                    $url .= '&';
+                }
+                $url .= "q=";
+                $url .= '(' . implode(' ) AND ( ', $values) . ')';
+                $first = false;
+            } else {
+                foreach ($values as $val) {
+                    if (! $first) {
+                        $url .= '&';
+                    }
+                    $val = (string) $val;
+                    $url .= "$key=$val";
+                    $first = false;
+                }
+            }
+        }
+        
+        return $url;
+    }
 }
